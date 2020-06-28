3879198f05ab73c751ef722186f19ed81b75ff9f
Robert Keszeg <keszegrobert@gmail.com>
2020-06-28 14:08:36 +0200

    iterating through the contents of the folder

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <dirent.h>
 #include <iostream>
 #include <lame/lame.h>
 
@@ -8,6 +9,24 @@ int main(int argc, char* argv[])
         std::cout << "  Usage: wav2mp3 folder" << std::endl;
         return EXIT_FAILURE;
     }
+
+    DIR* dir = opendir(argv[1]);
+    if (dir == nullptr) {
+        std::cout << "Folder does not exist:" << argv[1] << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    struct dirent* ent = readdir(dir);
+    while (ent != NULL) {
+        std::string filename(ent->d_name);
+        if (filename.rfind(".wav") == std::string::npos) {
+            ent = readdir(dir);
+            continue;
+        }
+        std::cout << ent->d_name << std::endl;
+        ent = readdir(dir);
+    }
+
     lame_t lame = lame_init();
     std::cout << "Hello, lame " << get_lame_version() << std::endl;
     lame_close(lame);
