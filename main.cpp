0748e6d1d51f123a2df5492a58737b68dcb34562
Robert Keszeg <keszegrobert@gmail.com>
2020-06-28 13:09:01 +0200

    added tests for the cmdline argument

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <lame/lame.h>
 
-int main(int, char**)
+int main(int argc, char* argv[])
 {
+    if (argc != 2) {
+        std::cout << "This tool converts a folder of wav files to mp3" << std::endl;
+        std::cout << "  Usage: wav2mp3 folder" << std::endl;
+        return EXIT_FAILURE;
+    }
     lame_t lame = lame_init();
-    std::cout << "Hello, lame,  " << get_lame_version() << std::endl;
+    std::cout << "Hello, lame " << get_lame_version() << std::endl;
     lame_close(lame);
+    return EXIT_SUCCESS;
 }
