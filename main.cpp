5bb550e8695e6761b08b3142aacd217f7251380d
Robert Keszeg <keszegrobert@gmail.com>
2020-06-28 14:54:40 +0200

    refactored the way we read the files from the dir

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <dirent.h>
 #include <iostream>
 #include <lame/lame.h>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
+
+const std::vector<std::string> get_wav_files(std::string folder)
+{
+    DIR* dir = opendir(folder.c_str());
+    if (dir == nullptr) {
+        std::stringstream ss;
+        ss << "Folder does not exist:" << folder << std::endl;
+        throw std::runtime_error(ss.str());
+    }
+    std::vector<std::string> wavs;
+    while (true) {
+        struct dirent* ent = readdir(dir);
+        if (ent == nullptr)
+            break;
+        std::string filename(ent->d_name);
+        if (filename.rfind(".wav") == std::string::npos) {
+            continue;
+        }
+        wavs.push_back(filename);
+    }
+    return wavs;
+}
 
 int main(int argc, char* argv[])
 {
@@ -9,22 +34,11 @@ int main(int argc, char* argv[])
         std::cout << "  Usage: wav2mp3 folder" << std::endl;
         return EXIT_FAILURE;
     }
-
-    DIR* dir = opendir(argv[1]);
-    if (dir == nullptr) {
-        std::cout << "Folder does not exist:" << argv[1] << std::endl;
+    try {
+        std::vector<std::string> paths = get_wav_files(argv[1]);
+    } catch (const std::exception& e) {
+        std::cout << e.what();
         return EXIT_FAILURE;
-    }
-
-    struct dirent* ent = readdir(dir);
-    while (ent != NULL) {
-        std::string filename(ent->d_name);
-        if (filename.rfind(".wav") == std::string::npos) {
-            ent = readdir(dir);
-            continue;
-        }
-        std::cout << ent->d_name << std::endl;
-        ent = readdir(dir);
     }
 
     lame_t lame = lame_init();
